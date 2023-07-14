c2f69b989538f422ed9b6b146e4a5f790f2f79c2
vidya <vidyashivakumar13@gmail.com>
2023-07-14 20:05:41 +0530

    changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("vidya");
+        System.out.println(obj.getname());
+
+
+
+    }
   
 }
